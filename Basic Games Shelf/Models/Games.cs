17f0f8d972d967120022c849ac327d1194cad63a
StyKon <khalilfrikha30@gmail.com>
2023-01-21 12:22:33 +0100

    Creating Models

---
+++ Basic Games Shelf/Models/Games.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Basic_Games_Shelf.Models
+{
+    public class Games
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public string Game { get; set; }
+        [Required]
+        public int PlayTime { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        [Required]
+        public string[] Platforms { get; set; }
+    }
+}
